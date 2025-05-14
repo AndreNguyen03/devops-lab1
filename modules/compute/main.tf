09678d0a8141ef6ea13a25ce138e5be754eda59e
Ngoc Anh <ngocanh030110@gmail.com>
2025-05-14 23:50:15 +0700

    fix var compute

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "public" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   subnet_id                   = var.public_subnet_id
-  vpc_security_group_ids      = [var.public_sg_id, vảr.restricted_sg_id]
+  vpc_security_group_ids      = [var.public_sg_id, var.restricted_sg_id]
   key_name                    = var.key_name
   associate_public_ip_address = false
   ebs_optimized               = true

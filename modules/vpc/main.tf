8b0b7161366c1a32bafbc3f84ff3edf891cdd2b8
Ngoc Anh <ngocanh030110@gmail.com>
2025-05-14 23:35:49 +0700

    fix vpc/count error

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,19 +4,13 @@ resource "aws_vpc" "main-vpc" {
     Name = "main-vpc"
   }
 }
-
-locals {
-  public_subnet_name = "public-subnet-${count.index + 1}"
-  private_subnet_name = "private-subnet-${count.index + 1}"
-}
-
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main-vpc.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   map_public_ip_on_launch = true
   tags = {
-    Name = local.public_subnet_name
+    Name ="public-subnet-${count.index + 1}"
   }
 }
 
@@ -26,7 +20,7 @@ resource "aws_subnet" "private" {
   cidr_block              = var.private_subnet_cidrs[count.index]
   map_public_ip_on_launch = false
   tags = {
-    Name = local.private_subnet_name
+    Name = "private-subnet-${count.index + 1}"
   }
 }
 
